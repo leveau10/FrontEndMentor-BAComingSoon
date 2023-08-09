43e90964a1bd1103c29414abb6e09ae62870e176
Leonardo Viana <76445679+leveau10@users.noreply.github.com>
2023-08-08 22:52:17 -0300

    validation done

---
+++ validation.js
@@ -0,0 +1,14 @@
+var emailInput = document.getElementById('email')
+emailInput.addEventListener("keyup", function () { ValidateEmail(emailInput) })
+
+
+function ValidateEmail(emailInput) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
+        document.getElementById('error-message').classList.remove('email-error-active');
+        document.getElementById('error-icon').classList.remove('error-icon-activated');
+    } else {
+        document.getElementById('error-message').classList.add('email-error-active');
+        document.getElementById('error-icon').classList.add('error-icon-activated');
+    }
+
+}
